09b05474735c2217994312e224a758914ea2d351
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-09-29 19:45:16 +0700

    add command/discussion

---
+++ src/commands/discussion.js
@@ -0,0 +1,25 @@
+const Command = require('../Command');
+
+const GameStages = require('../GameStages');
+const GameExistenceRequirement = require('../GameExistenceRequirement');
+const GameParticipationRequirement = require('../GameParticipationRequirement');
+
+module.exports = class Discussion extends Command {
+  constructor () {
+    super({
+      name: 'discussion',
+      aliases: [ 'ds' ],
+      description: 'Sets the stage to discussion, umnutes everyone who is alive',
+
+      gameExistenceRequirement: GameExistenceRequirement.GAME,
+      gameParticipationRequirement: GameParticipationRequirement.PARTICIPATING,
+      voiceChannelOnly: true
+    })
+  }
+
+  run ({ message, game, prefix }) {
+    if (!game) return message.channel.send(`No game, type \`${prefix}newgame\` to start one!`);
+    game.setStage(GameStages.DISCUSSION);
+    message.channel.send('Stage set to **discussion**');
+  }
+}
