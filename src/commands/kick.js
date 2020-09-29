f84eff230053b7d156161e3ef4f42d1874905924
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-09-29 21:13:18 +0700

    add command/kick

---
+++ src/commands/kick.js
@@ -0,0 +1,30 @@
+const Command = require('../Command');
+const Utils = require('../Utils');
+
+const ColorRequirement = require('../ColorRequirement');
+const GameExistenceRequirement = require('../GameExistenceRequirement');
+const GameParticipationRequirement = require('../GameParticipationRequirement');
+
+module.exports = class Join extends Command {
+  constructor() {
+    super({
+      name: 'kick',
+      aliases: ['k'],
+      usage: '<color>',
+      description: 'Removes a player from the game',
+
+      gameExistenceRequirement: GameExistenceRequirement.GAME,
+      colorRequirement: ColorRequirement.OCCUPIED
+    });
+  }
+
+  run({
+    message,
+    game
+  }) {
+    const color = message.content.split(' ')[1].toLowerCase();
+    const player = game.getPlayerByColor(color);
+    message.channel.send(`**${player.member.user.tag}** has been kicked from the game.`);
+    game.removePlayer(player.member);
+  }
+}
