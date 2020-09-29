b118b9ca9b76a5a10b8875c416c431d5243a7dde
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-09-29 21:58:50 +0700

    add command/gamecount

---
+++ src/commands/endgame.js
@@ -0,0 +1,25 @@
+const Command = require('../Command');
+
+const GameExistenceRequirement = require('../GameExistenceRequirement');
+
+module.exports = class EndGame extends Command {
+  constructor() {
+    super({
+      name: 'endgame',
+      aliases: ['eg'],
+      description: 'Ends the current game',
+
+      gameExistenceRequirement: GameExistenceRequirement.GAME
+    });
+  }
+
+  run({
+    message,
+    gameManager,
+    prefix,
+    voiceChannel
+  }) {
+    gameManager.endGame(voiceChannel);
+    message.channel.send(`Game ended. Start a new one with \`${prefix}newgame\``);
+  }
+}
