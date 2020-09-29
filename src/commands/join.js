7129cc0ba5646f99a0f3aeed21c786951a1775c7
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-09-29 21:43:58 +0700

    add command/join

---
+++ src/commands/join.js
@@ -0,0 +1,31 @@
+const Command = require('../Command');
+const Utils = require('../Utils');
+
+const ColorRequirement = require('../ColorRequirement');
+const GameExistenceRequirement = require('../GameExistenceRequirement');
+const GameParticipationRequirement = require('../GameParticipationRequirement');
+
+module.exports = class Join extends Command {
+  constructor() {
+    super({
+      name: 'join',
+      aliases: ['j'],
+      usage: '<color>',
+      description: 'Joins the current game as a color',
+
+      gameExistenceRequirement: GameExistenceRequirement.GAME,
+      gameParticipationRequirement: GameParticipationRequirement.NOT_PARTICIPATING,
+      colorRequirement: ColorRequirement.AVAILABLE
+    });
+  }
+
+  run({
+    message,
+    game,
+    emojis
+  }) {
+    const color = message.content.split(' ')[1].toLowerCase();
+    const player = game.addPlayer(message.member, color);
+    message.channel.send(`You joined the game as ${Utils.getPlayerEmoji(player, emojis)} \`${player.color}\``);
+  }
+}
