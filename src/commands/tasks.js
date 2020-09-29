bba61fa7e47c286f0a9ac0916ef59648058efa98
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-09-29 20:25:32 +0700

    add command/tasks

---
+++ src/commands/tasks.js
@@ -0,0 +1,27 @@
+const Command = require('../Command');
+
+const GameStages = require('../GameStages');
+const GameExistenceRequirement = require('../GameExistenceRequirement');
+const GameParticipationRequirement = require('../GameParticipationRequirement');
+
+module.exports = class Tasks extends Command {
+  constructor() {
+    super({
+      name: 'tasks',
+      aliases: ['ts'],
+      description: 'Sets the stage to tasks and mutes everyone',
+
+      gameExistenceRequirement: GameExistenceRequirement.GAME,
+      gameParticipationRequirement: GameParticipationRequirement.PARTICIPATING,
+      voiceChannelOnly: true
+    })
+  }
+
+  run({
+    message,
+    game
+  }) {
+    game.setStage(GameStages.TASKS);
+    message.channel.send('Stage set to **tasks**');
+  }
+}
