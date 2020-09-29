730ea31df0796a8ebfbf4f6cabc473b98c79e789
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-09-29 21:05:41 +0700

    add command/leave

---
+++ src/commands/leave.js
@@ -0,0 +1,26 @@
+const Command = require('../Command');
+
+const GameExistenceRequirement = require('../GameExistenceRequirement');
+const GameParticipationRequirement = require('../GameParticipationRequirement');
+
+module.exports = class Leave extends Command {
+  constructor() {
+    super({
+      name: 'leave',
+      aliases: ['l'],
+      description: 'Leaves the current game',
+
+      gameExistenceRequirement: GameExistenceRequirement.GAME,
+      gameParticipationRequirement: GameParticipationRequirement.PARTICIPATING,
+      voiceChannelOnly: true
+    });
+  }
+
+  run({
+    message,
+    game
+  }) {
+    message.channel.send(`You left the game`);
+    game.removePlayer(message.member);
+  }
+}
